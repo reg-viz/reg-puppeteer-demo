2a22fbf5d74aac885d289b962197a48d3f6c7c79
y-kurami <yosuke.kurami@gmail.com>
2017-12-04 17:09:15 +0900

    stabilize capturing

--- capture.js
+++ capture.js
@@ -7,6 +7,7 @@ async function main() {
 
   page.setViewport({ width: 640, height: 680 });
   await page.goto(`file://${__dirname}/index.html`);
+  await new Promise(res => setTimeout(() => res(), 300));
   await page.screenshot({ path: 'screenshot/index.png' });
 
   await page.close();
