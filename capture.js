16b1ccd75a38a1cd6fba302a9e9446b5e106fe1d
rhysd <lin90162@yahoo.co.jp>
2019-07-27 16:38:20 +0900

    add missing `await` to setViewport() calls

--- capture.js
+++ capture.js
@@ -5,7 +5,7 @@ async function main() {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   let page = await browser.newPage();
 
-  page.setViewport({ width: 640, height: 680 });
+  await page.setViewport({ width: 640, height: 680 });
   await page.goto(`file://${__dirname}/index.html`);
   await new Promise(res => setTimeout(() => res(), 300));
   await page.screenshot({ path: 'screenshot/index.png' });
